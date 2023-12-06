d86e4b36135a3bef1c11871541770ae548ce36ac
Joy <77735525+JOY2OP@users.noreply.github.com>
2023-12-06 14:53:41 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,29 @@
+let nextId=0;
+export default function todo(){
+    const [item, setItem] = useState("");
+    const [arr, setArr] = useState([]);
 
+    return(
+        <div>
+        <input type='text' value={item} onChange={e => setItem(e.target.value)} />
+        <button onClick={() => {setArr([...arr,{id: nextId++, name:item, } ])}}>Add</button>
+        <ul>
+           { arr.map(ar => 
+           <li key={ar.id}>
+                {ar.name} {" "}
+                <button onClick={() => {setArr(arr.filter(a => a.id !== ar.id))}}>Delete</button>
+                {/* If true, the element is included in the new array; if false, the element is excluded. */}
+                {/* 
+                Element(a)	a.id	ar.id	a.id !== ar.id
+                Element 1	0	        2	true
+                Element 2	1	        2	true
+                Element 3	2	        2	false       
+                Element 4	3	        2	true 
+                */}
+           </li>)}
+           
+        </ul>
+        </div>
+        
+    )
+}
